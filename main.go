2bd75fbfa147fba21772e4ca156556df08c7941b
Alex26rus-api <mechkaev001@gmail.com>
2025-11-21 22:07:30 +0300

    дз сделал

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
+func readInput() (float64, string, string) {
+	var amount float64
+	var from string
+	var to string
+	fmt.Print("Введите сумму: ")
+	fmt.Scanln(&amount)
+	fmt.Print("Введите исходную валюту: ")
+	fmt.Scanln(&from)
+	fmt.Print("Введите целевую валюту: ")
+	fmt.Scanln(&to)
+	return amount, from, to
+}
+
+func Convert(amount float64, from string, to string) float64 {
+
+	return 0
+}
+
 func main() {
-
-	const USD_TO_EUR = 0.92
-	const USD_TO_RUB = 95.50
-
-	eurToRub := (1 / USD_TO_EUR) * USD_TO_RUB
-
-	fmt.Printf("1 EUR = %.2f RUB\n", eurToRub)
-
+	amount, from, to := readInput()
+	result := Convert(amount, from, to)
+	fmt.Println(result)
 }
