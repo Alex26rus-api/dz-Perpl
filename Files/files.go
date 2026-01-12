d65fe364565a29756ede050e7d424765bcf05082
Alex26rus-api <mechkaev001@gmail.com>
2026-01-12 14:51:41 +0300

    методы в пакетах

--- Files/files.go
+++ Files/files.go
@@ -1,3 +1,38 @@
 package files
 
-// TODO: чтение и запись файлов
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func IsJson(name string) bool {
+	ext := filepath.Ext(name)
+	ext = strings.ToLower(ext)
+	if ext == ".json" {
+		return true
+	}
+	return false
+}
+
+func ReadFile(name string) ([]byte, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func WriteFile(name string, data []byte) error {
+	file, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- Storage/storage.go
+++ Storage/storage.go
@@ -1,3 +1,38 @@
 package storage
 
-// TODO: локальное хранилище bin
+import (
+	bins "dz/app/Bins"
+	files "dz/app/Files"
+	"encoding/json"
+	"errors"
+)
+
+func SaveBins(fileName string, b bins.BinList) error {
+	data, err := json.MarshalIndent(b, "", " ")
+	if err != nil {
+		return err
+	}
+	err = files.WriteFile(fileName, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadBins(fileName string) (bins.BinList, error) {
+	path := files.IsJson(fileName)
+	if path == false {
+		err := errors.New("file is not a json")
+		return bins.BinList{}, err
+	}
+	data, err := files.ReadFile(fileName)
+	if err != nil {
+		return bins.BinList{}, err
+	}
+	var bin bins.BinList
+	err = json.Unmarshal(data, &bin)
+	if err != nil {
+		return bins.BinList{}, err
+	}
+	return bin, nil
+}
