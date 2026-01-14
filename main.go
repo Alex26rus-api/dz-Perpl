ba65f1d1f4d32b9ff82d3ee971a2c90931511912
Alex26rus-api <mechkaev001@gmail.com>
2026-01-14 16:32:34 +0300

    Connect bins storage in main

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	bins "dz/app/Bins"
+	storage "dz/app/Storage"
 	"fmt"
 )
 
@@ -9,6 +10,15 @@ func main() {
 	list := bins.BinList{}
 	bin := bins.NewBin("1", "config.json", false)
 	list.Add(bin)
+
+	err := storage.SaveBins("bins.json", list)
+	if err != nil {
+		return
+	}
+	list, err = storage.LoadBins("bins.json")
+	if err != nil {
+		return
+	}
 
 	fmt.Println(list)
 }
